41cca5a81f20da8bcaf8f7f884d3beebbd81c973
VincentChinGuan <lanfong0765@gmail.com>
2026-06-16 18:22:26 +0800

    test

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <script src="https://google.com" async defer></script>
+</head>
+<body>
+    <div class="top-container">
+        <h1> Welcome back </h1>
+        <h5> Lorem ipsum dolor sit amet, consectetuer adipiscing elit </h5>
+    </div>
+    <div class="g_id_signin"
+         data-type="standard"
+         data-shape="rectangular"
+         data-theme="outline"
+         data-text="signin_with"
+         data-size="large"
+         data-logo_alignment="left">
+    </div>
+
+</body>
+</html>
