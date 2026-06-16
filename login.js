11d38e50c9023fec2c71000780eb64f642ce535f
VincentChinGuan <lanfong0765@gmail.com>
2026-06-16 22:25:25 +0800

    test

---
+++ login.js
@@ -0,0 +1,33 @@
+function parseJwt (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+
+        return JSON.parse(jsonPayload);
+    }
+
+function handleCredentialResponse(response) {
+        
+    const userProfile = parseJwt(response.credential);
+    console.log(response.credential);
+    localStorage.setItem("userName", userProfile.name);
+    localStorage.setItem("userEmail", userProfile.email);
+    localStorage.setItem("userPicture", userProfile.picture);
+
+    window.location.href = "/dashboard.html";
+        }
+
+window.onload = function () {
+    google.accounts.id.initialize({
+        client_id: "715062166189-7vv10mq414ileois6cqs13jg9prk4485.apps.googleusercontent.com", 
+        callback: handleCredentialResponse
+    });
+
+    google.accounts.id.renderButton(
+        document.getElementById("googleBtn"), { theme: "filled_blue", size: "large", width: 260 } 
+    );
+
+    google.accounts.id.prompt(); 
+}
