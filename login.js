2b46aa61b69c79d908b7b4fe9c7b2943527428d9
VincentChinGuan <lanfong0765@gmail.com>
2026-06-19 19:12:31 +0800

    test

--- login.js
+++ login.js
@@ -21,7 +21,7 @@ function handleCredentialResponse(response) {
 
 window.onload = function () {
     google.accounts.id.initialize({
-        client_id: process.env.Google_API || "process.env.Google_API",
+        client_id: process?.env?.NEXT_PUBLIC_GOOGLE_API ,
         callback: handleCredentialResponse
     });
 
