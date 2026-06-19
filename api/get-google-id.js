0a8a07b7dc2b53b400237f4636cb90e40924b669
VincentChinGuan <lanfong0765@gmail.com>
2026-06-19 19:25:07 +0800

    test

--- api/get-google-id.js
+++ api/get-google-id.js
@@ -1,9 +1,9 @@
-export default function handler(request, response) {
-  // This securely grabs the key from your Vercel dashboard settings
-  const clientId = process.env.NEXT_PUBLIC_GOOGLE_API;
+module.exports = function handler(request, response) {
+  // Securely grabs the key from your Vercel settings
+  const clientId = process.env.GOOGLE_API;
 
-  // This sends the key back to your frontend code safely
+  // Sends the key back to your frontend code
   response.status(200).json({
     client_id: clientId
   });
-}
+};
